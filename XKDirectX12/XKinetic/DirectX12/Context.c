e2201777619a1240f6de08c143344c1c99d803d7
UnknownSadPepe <unknownsadpepe@gmail.com>
2022-06-13 09:21:42 -0700

    daily commit

---
+++ XKDirectX12/XKinetic/DirectX12/Context.c
@@ -0,0 +1,17 @@
+#include "XKinetic/DirectX12/Internal.h"
+#include "XKinetic/Platform/Window.h"
+
+__XkDX12Context _xkDX12Context;
+
+XkResult __xkDX12InitializeContext(void) {
+  XkResult result = XK_SUCCESS;
+
+  /// TODO: implementation.
+
+_catch:
+  return(result);
+}
+
+void __xkDX12TerminateContext(void) {
+  /// TODO: implementation.
+}
--- XKDirectX12/XKinetic/DirectX12/Internal.h
+++ XKDirectX12/XKinetic/DirectX12/Internal.h
@@ -3,3 +3,9 @@
 #include "XKinetic/XKDirectX12.h"
 #include "XKinetic/Core/Minimal.h"
 #include "XKinetic/Platform/Window.h"
+
+typedef struct {
+
+} __XkDX12Context;
+
+extern __XkDX12Context _xkDX12Context;
